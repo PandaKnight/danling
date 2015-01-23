11644612f001e910c1b55602967d7d87ed4c942a
葛威 <gewei@lakala.com>
2015-01-23 14:34:39 +0800

    添加Application.js

---
+++ js/application.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Michael on 15-1-21.
+ */
+
+/**
+ * 当应用准备就绪可以运行时调用此方法，此方法是内可做一些应用初始化的操作。
+ * @param application DL.Applicaton 对象实例
+ */
+function application_lanuch(application){
+
+}
+
+/**
+ * 当应用从后台或从其它页面切换过来时调用此方法。
+ * @param application  DL.Applicaton 对象实例
+ */
+function application_resume(application){
+
+}
+
+/**
+ * 当应用切到后台或切换到其它页面时调用此方法
+ * @param application  DL.Applicaton 对象实例
+ */
+function application_pause(application){
+
+}
--- js/lib/danling.js
+++ js/lib/danling.js
@@ -1,3 +1,48 @@
 /**
  * Created by Michael on 15-1-21.
  */
+
+/**
+ * 将 Ext 包导入到 DL
+ * @type {Ext|*}
+ */
+var DL = Ext;
+
+DL.application({
+    name: 'danlingapp',
+    launch: function() {
+        /*
+         Application.log('engineName=' + Ext.browser.engineName);
+         Application.log('engineVersion=' + Ext.browser.engineVersion);
+         Application.log('EngineVersion=' + Ext.browser.engineVersion);
+         Application.log('isWebView=' + Ext.browser.is['WebView']);
+         Application.log('haveCordova=' + Ext.browser.is['Cordova']);
+
+         Application.log('deviceType=' + Ext.os.deviceType);
+         Application.log('osName=' + Ext.os.name);
+         Application.log('OSVersion=' + Ext.os.version.getMajor());
+
+         Application.log('transitionEnd=' + Ext.browser.getVendorProperyName('transitionEnd'));
+         Application.log('animationStart=' + Ext.browser.getVendorProperyName('animationStart'));
+         Application.log('animationEnd=' +  Ext.browser.getVendorProperyName('animationEnd'));
+         */
+
+        document.addEventListener('deviceready', function(){
+            // cordova device ready
+            document.addEventListener('resume', function(){
+                if (application_pause != undefined){
+                    application_pause(this);
+                }
+            }, false);
+            document.addEventListener('pause', function(){
+                if (application_resume != undefined){
+                    application_resume(this);
+                }
+            }, false);
+
+            if (application_lanuch != undefined){
+                application_lanuch(this);
+            }
+        }, false);
+    }
+});
